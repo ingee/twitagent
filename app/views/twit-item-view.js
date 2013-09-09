2efddda4c610e3be38efa6284d8cc5ce9f58d76d
William Kim <azamara@me.com>
2013-09-09 19:43:01 +0900

    리스트뷰에 리스트 스타일이 나오도록 class 추가

--- app/views/twit-item-view.js
+++ app/views/twit-item-view.js
@@ -6,7 +6,7 @@
 function(Backbone, TwitItemTemplate) {
 
   var TwitItemView = Backbone.View.extend({
-    tagName: 'div',
+    tagName: 'li', // 부모 엘리먼트가 ul이므로 리스트 아이템은 li 엘리먼트로 변경
 
     render: function() {
       this.$el.html(TwitItemTemplate({ twit: this.model.toJSON() }));
