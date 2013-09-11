4c7aa18f8a35cbb3bc6ce94cc9a979e5db457a9c
Kim, In Gee <ingee@paran.com>
2013-09-12 01:18:50 +0900

    make group-page-view (for Twitter List)

--- app/mainrouter.js
+++ app/mainrouter.js
@@ -1,15 +1,22 @@
 ﻿define( [
   'backbone',
   'views/twit-page-view',
+  'views/group-page-view',
   'multipage-router'
 ],
 
-function(Backbone, TwitPageView, MultipageRouter) {
+function(
+  Backbone,
+  TwitPageView,
+  GroupPageView,
+  MultipageRouter
+)
+{
 
   return {
     launch: function() {
       console.log('application launch() started');
-      
+
       var MainRouter = MultipageRouter.extend( {
         pages: {
           'twit-page': {
@@ -21,25 +28,29 @@ function(Backbone, TwitPageView, MultipageRouter) {
           },
           'group-page': {
             fragment: [ 'groups' ],
-            el: '#group-vw'
+            el: '#group-vw',
+            render: function() {
+              this.groupPageView.render();
+            }
           },
           'default': {
             el: '#default-vw'
           }
         },
-        
+
         transitions: {
           'twit-page:group-page': 'flip',
           'twit-page:default': 'flip',
           'group-page:default': 'flip'
         },
-        
+
         initialize: function() {
           console.log('router.initialize()');
           this.twitPageView = new TwitPageView();
+          this.groupPageView = new GroupPageView();
         }
       } );
-    
+
       new MainRouter();
       Backbone.history.start();
     }
---
+++ app/views/group-page-view.js
@@ -0,0 +1,36 @@
+﻿define([
+  'backbone',
+  'template!templates/group-page-view',
+  'widget-listview'
+],
+
+function(
+  Backbone, 
+  GroupPageTemplate, 
+  ListView
+) {
+
+  var GroupPageView = Backbone.View.extend({
+    el: '#group-vw',
+
+    render: function() {
+      //this.listView.render();
+      return this;
+    },
+
+    initialize: function() {
+      this.$el.html(GroupPageTemplate());
+      /*
+      this.listView = new ListView({
+        el: '#twit-list',
+        collection: new TwitCollection(),
+        itemView: TwitItemView,
+        optimization: false
+      });
+      */
+    }
+  });
+
+  return GroupPageView;
+
+});
