ad3e9b2384ab5dca0c855f62b36506eba02ff871
William Kim <azamara@me.com>
2013-09-12 02:24:27 +0900

    홈 버튼으로 접속시 계속 리스트가 쌓이는 문제 수정

--- app/views/twit-page-view.js
+++ app/views/twit-page-view.js
@@ -17,19 +17,31 @@ function(
   var TwitPageView = Backbone.View.extend({
     el: '#twit-vw',
 
+    isFirst: true,
+
     render: function() {
-      this.listView.render();
+      var self = this;
+
+      // this.listView가 존재하지 않은 경우만 new로 생성
+      this.listView = this.listView || new ListView({
+        el: '#twit-list',
+        collection: new TwitCollection(),
+        itemView: TwitItemView,
+        optimization: false,
+        scrollEndAction: function() {
+          console.log("scrollEnd Callback");
+          self.listView.render();
+        }
+      });
+
+      // 최초에만 listView 렌더링 리스트뷰의 렌더링은 itemView를 계속 append.
+      this.isFirst && this.listView.render();
+      this.isFirst = false;
       return this;
     },
 
     initialize: function() {
       this.$el.html(TwitPageTemplate());
-      this.listView = new ListView({
-        el: '#twit-list',
-        collection: new TwitCollection(),
-        itemView: TwitItemView,
-        optimization: false
-      });
     }
   });
 
