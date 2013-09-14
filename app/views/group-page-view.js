afc6b5371866e5812cc05d810efc7f73c6abb45c
Kim, In Gee <ingee@paran.com>
2013-09-15 04:54:51 +0900

    make group-view

--- app/views/group-page-view.js
+++ app/views/group-page-view.js
@@ -1,33 +1,40 @@
 ﻿define([
   'backbone',
   'template!templates/group-page-view',
+  'views/group-item-view',
+  'models/twit-collection',
   'widget-listview'
 ],
 
 function(
   Backbone, 
   GroupPageTemplate, 
+  GroupItemView,
+  TwitCollection,
   ListView
 ) {
 
   var GroupPageView = Backbone.View.extend({
     el: '#group-vw',
+    isFirst: true,
 
     render: function() {
-      //this.listView.render();
+      // 최초에만 listView 렌더링 리스트뷰의 렌더링은 itemView를 계속 append.
+      if (this.isFirst) {
+        this.listView.render();
+        this.isFirst = false;
+      }
       return this;
     },
 
     initialize: function() {
       this.$el.html(GroupPageTemplate());
-      /*
       this.listView = new ListView({
-        el: '#twit-list',
+        el: '#group-list',
         collection: new TwitCollection(),
-        itemView: TwitItemView,
+        itemView: GroupItemView,
         optimization: false
       });
-      */
     }
   });
 
---
+++ app/views/group-view.js
@@ -0,0 +1,19 @@
+﻿define([
+  'backbone',
+  'template!templates/group-item-view'
+],
+
+function(Backbone, GroupItemTemplate) {
+
+  var GroupItemView = Backbone.View.extend({
+    tagName: 'li',
+
+    render: function() {
+      this.$el.html(GroupItemTemplate(this.model.toJSON()));
+      return this;
+    }
+  });
+
+  return GroupItemView;
+
+});
