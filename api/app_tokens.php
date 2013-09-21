471789b6b4c37b0269e7e0f497ac4b30637975f5
Kim, In Gee <ingee@paran.com>
2013-09-22 02:04:39 +0900

    make Twitter API proxy with PHP

---
+++ api/app_tokens.php
@@ -0,0 +1,11 @@
+<?php
+
+// OAuth tokens for single-user programming
+// After you create an app you MUST copy 
+// the tokens and paste them into the following lines
+$consumer_key = 'CONSUMER KEY';
+$consumer_secret = 'CONSUMER SECRET';
+$user_token = 'USER TOKEN';
+$user_secret = 'USER SECRET';
+
+?>
